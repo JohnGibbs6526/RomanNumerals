6a3348d7f7ac83de48c47db20b4dbba10613921a
JohnGibbs6526 <JohnGibbs6526@mail.davistech.edu>
2025-04-15 12:17:31 -0600

    create Roman Numerals project

---
+++ RomanNumerals/Numerals.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace RomanNumerals
+{
+    public static class Numerals
+    {
+        static void SplitNumber(int number, out int thousands, out int hundreds, out int tens, out int ones)
+        {
+            // Split number into thousands, hundreds, tens, and ones
+
+            int curr = number;
+
+            thousands = curr / 1000;
+            curr -= thousands * 1000;
+
+            hundreds = curr / 100;
+            curr -= hundreds * 100;
+
+            tens = curr / 10;
+            curr -= tens * 10;
+
+            ones = curr;
+            curr -= ones;
+
+            WriteLine("\nThousands: " + thousands);
+            WriteLine("Hundreds: " + hundreds);
+            WriteLine("Tens: " + tens);
+            WriteLine("Ones: " + ones);
+        }
+        public static string ArabicToRoman(int number)
+        {
+            int thousands, hundreds, tens, ones;
+            SplitNumber(number, out thousands, out hundreds, out tens, out ones);
+
+            int curr;
+            string numeral = "";
+
+            // Append numeral
+
+            // Thousands
+
+            for (int i = 0; i < thousands; i++)
+            {
+                numeral += "M";
+            }
+
+            // Hundreds
+
+            curr = hundreds;
+            if (curr == 9)
+            {
+                numeral += "CM";
+            }
+            else if (curr == 4)
+            {
+                numeral += "CD";
+            }
+            else
+            {
+                if (curr >= 5)
+                {
+                    curr -= 5;
+                    numeral += "D";
+                }
+
+                for (int i = 0; i < curr; i++)
+                {
+                    numeral += "C";
+                }
+            }
+
+            // Tens
+
+            curr = tens;
+            if (curr == 9)
+            {
+                numeral += "XC";
+            }
+            else if (curr == 4)
+            {
+                numeral += "XL";
+            }
+            else
+            {
+                if (curr >= 5)
+                {
+                    curr -= 5;
+                    numeral += "L";
+                }
+
+                for (int i = 0; i < curr; i++)
+                {
+                    numeral += "X";
+                }
+            }
+
+            // Ones
+
+            curr = ones;
+            if (curr == 9)
+            {
+                numeral += "IX";
+            }
+            else if (curr == 4)
+            {
+                numeral += "IV";
+            }
+            else
+            {
+                if (curr >= 5)
+                {
+                    curr -= 5;
+                    numeral += "V";
+                }
+
+                for (int i = 0; i < curr; i++)
+                {
+                    numeral += "I";
+                }
+            }
+
+            return numeral;
+        }
+    }
+}
---
+++ RomanNumerals/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace RomanNumerals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Roman Numeral Conversion\n");
+
+            char again;
+
+            do
+            {
+                try
+                {
+                    // Input option
+                    WriteLine("\nDo you want to convert Arabic numerals to Roman numerals (a)");
+                    Write("or Roman numerals to Arabic numerals (r)? ");
+                    char option = char.Parse(ReadLine());
+
+                    if (option == 'a')
+                    {
+                        // Input Arabic numeral
+                        Write("Enter an integer (1 to 3999): ");
+                        int number = int.Parse(ReadLine());
+
+                        if (number < 1 || number > 3999)
+                        {
+                            throw new Exception("Number is not between 1 and 3999. Please try again.");
+                        }
+
+                        // Compute Roman numeral
+                        string numeral = Numerals.ArabicToRoman(number);
+                        WriteLine("\nNumeral: " + numeral);
+                    }
+                    else if (option == 'r')
+                    {
+                        WriteLine("Coming soon...");
+                    }
+                    else
+                    {
+                        throw new Exception("Not a valid option.");
+                    }
+
+                    Write("Do you want to try again? (y/n) ");
+                    again = char.Parse(ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    WriteLine("  " + ex.Message);
+                    again = 'y';
+                }
+            }
+            while (again == 'y');
+
+            Write("\nPress any key to continue... ");
+            ReadKey();
+        }
+    }
+}
